4903422ffd9d53eadd0819baa3b5d646fcfc4f8d
uwinezagloria <gloria7wineza@gmail.com>
2025-06-24 15:50:32 +0200

    objects

---
+++ Objects/listProperties.js
@@ -0,0 +1,9 @@
+//Write a JavaScript program to list the properties of a JavaScript object.
+var person={
+    firstName:"Gloria",
+    secondName:"Uwineza",
+    gender:"female"
+}
+for(key in person){
+    console.log(key)
+}
---
+++ Objects/readingStatus.js
@@ -0,0 +1,2 @@
+/* Write a JavaScript program to display the reading status 
+(i.e. display book name, author name and reading status) of the following books */
