57f0dfe7194d895207831bcf8fd546f2e346a5e4
uwinezagloria <gloria7wineza@gmail.com>
2025-06-27 14:49:44 +0200

    type coercion ,reduce method

--- review.js
+++ review.js
@@ -24,3 +24,18 @@ console.log(undefined === undefined)
 console.log(null == undefined)
 console.log(NaN==NaN)
 console.log(undefined == undefined)
+console.log(Number("123.89"))
+console.log(parseInt("23.9"))
+console.log(parseFloat("23.6"))
+console.log(+("44"))
+console.log(String(200))
+console.log((11).toString())
+console.log(true.toString())
+console.log(!"")
+let a=4;
+a="uwineza"
+var c=3;
+var c=6;
+var arr=[1,12,3]
+var sum=arr.reduce((curr,acc)=>{return acc+curr})
+console.log(sum)
