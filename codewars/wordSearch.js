eda96bd68f0e0f135332b251a6d3f00fb61a5da8
uwinezagloria <gloria7wineza@gmail.com>
2025-06-27 15:14:03 +0200

    function to search word in array

--- codewars/wordSearch.js
+++ codewars/wordSearch.js
@@ -1,10 +1,11 @@
 function wordSearch(query, seq){
   var arr=[]
   seq.forEach((element)=>{
-    if(element.includes(query)){
+    if(element.toLowerCase().includes(query.toLowerCase())){
       arr.push(element)
     }
+    
   })
- return arr 
+ return arr.length>0 ? arr:["Empty"]
 }
 console.log(wordSearch("me",["merry","home","cook"]))
