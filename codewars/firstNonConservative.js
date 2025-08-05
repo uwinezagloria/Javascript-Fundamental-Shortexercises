390169eb2377b63b89e9363698f0bd7fdb9725bd
uwinezagloria <gloria7wineza.com>
2025-08-05 17:39:58 +0200

    first non consecutive  codewar kata

---
+++ codewars/firstNonConservative.js
@@ -0,0 +1,13 @@
+function firstNonConsecutive(arr) {
+  if (arr.length < 2) return null;
+
+  arr.sort((a, b) => a - b); 
+
+  for (let i = 0; i < arr.length - 1; i++) {
+    if (arr[i + 1] !== arr[i] + 1) {
+      return arr[i + 1]; 
+    }
+  }
+
+  return null; 
+}
