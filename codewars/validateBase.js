1bd0b1bdf6a8de8a269f04bcd8a24d5747fb0d39
uwinezagloria <gloria7wineza@gmail.com>
2025-08-21 16:42:40 +0200

    Check whether a number is valid in a given numeral system codewar kata

---
+++ codewars/validateBase.js
@@ -0,0 +1,16 @@
+function validateBase(num, base) {
+  const letters={"0":0,"1":1,"2":2,"3":3,"4":4,"5":5,"6":6,"7":7,"8":8,"9":9, A : 10, B : 11,C : 12, D : 13, E : 14, F : 15, G : 16, H : 17, I : 18, J :19, K : 20, L : 21, M : 22, N : 23,
+      O : 24, P : 25, Q : 26, R : 27, S : 28, T : 29, U :30, V : 31,W : 32,X : 33,Y : 34,Z : 35}
+
+  num=num.split("")
+  var arr=[];
+ for(let char of num){
+  for(let obj in letters){
+    if(char=== obj){
+      arr.push( letters[obj])
+    }
+  }
+ }
+  var maximum=Math.max(...arr)
+  return maximum <base? true:false;
+}
