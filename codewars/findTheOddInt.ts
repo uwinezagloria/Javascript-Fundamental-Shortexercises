49c48fa0b9b6a72d80210c3be6dc6754aa2fcd84
uwinezagloria <gloria7wineza@gmail.com>
2026-02-03 17:45:31 +0200

    Finding Odd int 6kyu codewar kata

---
+++ codewars/findTheOddInt.ts
@@ -0,0 +1,19 @@
+export const findOdd = (xs: number[]): number => {
+  let obj:Record<number,number>={}
+let count=0
+let result=[]
+for(let elem of xs){
+  if(obj[elem]){
+    obj[elem]++
+  }
+  else{
+    obj[elem]=1
+  }
+}
+for(let item in obj){
+  if(obj[item]%2!==0){
+    result.push(item)
+  }
+}
+ return Number(result.join(""));
+};
