8266cd3a06b2d0d939d236c664f4755bf3c0e1a7
uwinezagloria <gloria7wineza@gmail.com>
2026-04-17 15:56:39 +0200

    area of square

--- codewars/maximumTripleSum.ts
+++ codewars/maximumTripleSum.ts
@@ -3,3 +3,4 @@ let uniqueNumber=[...new Set(nums)].sort((a,b)=>b-a)
 return uniqueNumber[0]+uniqueNumber[1]+uniqueNumber[2]
 
 }
+S
---
+++ codewars/squareArea.ts
@@ -0,0 +1,3 @@
+export const squareArea = (num : number) : number =>  {
+return ((4*num*num)/(Math.PI*Math.PI));
+  }
